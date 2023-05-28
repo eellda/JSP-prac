b282a3545613883836603809124b01bc8fc351f7
eellda <tree500253@gmail.com>
2023-05-29 04:52:44 +0900

    query test

--- src/main/java/com/example/jspprac/HelloServlet.java
+++ src/main/java/com/example/jspprac/HelloServlet.java
@@ -1,27 +1,28 @@
 package com.example.jspprac;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
+        String temp = req.getParameter("cnt");
 
-    public void destroy() {
+        int cnt = 10;
+
+        if (temp != null && !temp.equals("")) cnt = Integer.parseInt(temp);
+
+        for (int i = 0; i < cnt; i++) {
+            out.println((i + 1) + ": 안녕 servlet <br >");
+        }
     }
 }
