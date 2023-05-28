b8bf3085223bf8831fd002bcf5e014784ed15926
eellda <tree500253@gmail.com>
2023-05-29 07:36:15 +0900

    Fix filter

--- src/main/java/com/example/jspprac/filter/CharacterEncodingFilter.java
+++ src/main/java/com/example/jspprac/filter/CharacterEncodingFilter.java
@@ -1,8 +1,10 @@
 package com.example.jspprac.filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
+@WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
