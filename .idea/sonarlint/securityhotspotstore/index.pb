c2e1c1a99bc3e71acb64f8cc0765658bc13f71be
eellda <tree500253@gmail.com>
2023-05-29 05:50:39 +0900

    Filter test

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,9 +1,3 @@
 
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-I
-src/main/webapp/index.jsp,8\e\8e6d3b70c9f5b02570779b0441540d45eceb0ded
-c
-3src/main/java/com/example/jspprac/HelloServlet.java,c\2\c296e7c2f547dfb325fc98eee53908051981e1f2
-J
-src/main/webapp/index.html,1\3\1337d72a3b1928dba82aa5210f5cdc0e88ce895b
---
+++ src/main/java/com/example/jspprac/filter/CharacterEncodingFilter.java
@@ -0,0 +1,15 @@
+package com.example.jspprac.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CharacterEncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        chain.doFilter(request, response);
+        System.out.println("character filter");
+    }
+}
