a16cf226a90c74871222d1c44e0f548bbb2e2411
eellda <tree500253@gmail.com>
2023-05-29 21:43:54 +0900

    Calcpage test

--- src/main/java/com/example/jspprac/Servlet/Calc3Suvlet.java
+++ src/main/java/com/example/jspprac/Servlet/Calc3Suvlet.java
@@ -1,5 +1,8 @@
 package com.example.jspprac.Servlet;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,71 +15,37 @@ public class Calc3Suvlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html; charset=UTF-8");
-
-        ServletContext application = req.getServletContext();
-        HttpSession session = req.getSession();
         Cookie[] cookies = req.getCookies();
 
-        String v_ = req.getParameter("v");
-        String op = req.getParameter("operator");
+        String value = req.getParameter("value");
+        String operator = req.getParameter("operator");
+        String dot = req.getParameter("dot");
+        String exp = "";
 
-        PrintWriter out = resp.getWriter();
-
-        int v = 0;
-        int x = 0;
-
-        if (!v_.equals("")) {
-            v = Integer.parseInt(v_);
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("exp")) {
+                    exp = c.getValue();
+                    break;
+                }
+            }
         }
 
-        if (op.equals("=")) {
-            //int x = (int) application.getAttribute("value");
-            //int x = (int) session.getAttribute("value");
-
-            for (Cookie c : cookies) {
-                if (c.getName().equals("value")) {
-                    x =  Integer.parseInt(c.getValue());
-                    break;
-                }
+        if (operator != null && operator.equals("=")) {
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+            try {
+                exp = String.valueOf(engine.eval(exp));
+            } catch (ScriptException e) {
+                throw new RuntimeException(e);
             }
-
-            //String operator = (String) application.getAttribute("op");
-            //String operator = (String) session.getAttribute("op");
-            String operator = "";
-            int y = v;
-            int res = 0;
-            for (Cookie c : cookies) {
-                if (c.getName().equals("op")) {
-                    operator = c.getValue();
-                    break;
-                }
-            }
-
-            if (operator.equals("+")) {
-                res = x + y;
-            } else {
-                res = x - y;
-            }
-            out.printf("result: " + res);
         } else {
-//            application.setAttribute("value", v);
-//            application.setAttribute("op", op);
-
-            // session.setAttribute("value", v);
-            // session.setAttribute("op", op);
-
-            Cookie valueCookie = new Cookie("value", String.valueOf(v));
-            Cookie opCookie = new Cookie("op", op);
-            valueCookie.setPath("/calc2"); // 모든 페이지를 요청할 때마다 이 쿠키는 가져오라는 뜻
-            valueCookie.setMaxAge(600); // 보관 기간
-            opCookie.setPath("/calc2");
-            resp.addCookie(valueCookie);
-            resp.addCookie(opCookie);
-
-            resp.sendRedirect("calc2.html"); // 페이지 전환
+            exp += (value == null) ? "" : value;
+            exp += (operator == null) ? "" : operator;
+            exp += (dot == null) ? "" : dot;
         }
 
+        Cookie expCookie = new Cookie("exp", exp);
+        resp.addCookie(expCookie);
+        resp.sendRedirect("calcpage"); // 페이지 전환
     }
 }
