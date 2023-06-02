99750fb9cc701e4c2863c029bb349a9c6b8df682
eellda <tree500253@gmail.com>
2023-06-02 16:42:09 +0900

    notice CRUD 준비

--- src/main/java/com/example/jspprac/service/NoticeService.java
+++ src/main/java/com/example/jspprac/service/NoticeService.java
@@ -291,4 +291,30 @@ public class NoticeService {
 
         return notice;
     }
+
+    // 게시글
+    public int removeNoticeAll(int[] ids) {
+        return 0;
+    }
+
+    public int pubNoticeAll(int[] ids) {
+        return 0;
+    }
+
+    public int insertNotice(Notice notice) {
+        return 0;
+    }
+
+    public int deleteNotice(int id) {
+        return 0;
+    }
+
+    public int updateNotice(Notice notice) {
+        return 0;
+    }
+
+    public List<Notice> getNoticeNewestList() {
+        return null;
+    }
+
 }
